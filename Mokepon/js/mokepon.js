09a90ab603a15b03328433e1e391605b2a7994cc
Angie Zamora <angiezamora3101@gmail.com>
2025-12-15 16:19:25 -0500

    primera parte clase 19

--- Mokepon/js/mokepon.js
+++ Mokepon/js/mokepon.js
@@ -1,2 +1,11 @@
-alert("Hola, mundo JS")
-let botonMascotaJugador = document.getElementById("Boton-mascota")
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+}
+
+function seleccionarMascotaJugador() {
+    alert('SELECCIONASTE TU MASCOTA')
+}
+
+
+window.addEventListener('load', iniciarJuego)
